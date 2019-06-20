90a27325989c879cbb835bb6394686fcc4c0af41
Ashu Aditya <ashuaditya.developer@gmail.com>
2019-06-20 22:03:43 +0530

    1st Commit

---
+++ src/main/java/com/app/SpringBootDataJpaPaginationWithStreamApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaPaginationWithStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaPaginationWithStreamApplication.class, args);
+	}
+
+}
